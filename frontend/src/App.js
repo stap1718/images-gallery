6830471c731cec7a86174bc22a6a0b81e3cb55fb
Phillip Stapleton <stapletonp@cbchs.org>
2023-10-09 12:58:11 -0500

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+import { useState } from 'react';
+
+
+
 
 const App = ()  => {
-  return (
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+ return (
     <div>
       <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
