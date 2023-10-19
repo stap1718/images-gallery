fc9c438af1b7e32a20a41488aef605afd5c6dd9a
Phillip Stapleton <stapletonp@cbchs.org>
2023-10-19 11:39:06 -0500

    made changes to eslint and prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,24 @@ import { useState } from 'react';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-
-const App = ()  => {
+const App = () => {
   const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
     setWord('');
-  }
+  };
 
   return (
     <div>
@@ -29,6 +30,6 @@ const App = ()  => {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
