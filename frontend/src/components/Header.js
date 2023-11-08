42b4d0004686f0cfc990c45683085330c8f20d82
Phillip Stapleton <stapletonp@cbchs.org>
2023-11-08 12:50:14 -0600

    added text logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,15 +5,16 @@ import Container from 'react-bootstrap/Container';
 //import Nav from 'react-bootstrap/Nav';
 //import Form from 'react-bootstrap/Form';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
+import { ReactComponent as Logo } from '../images/logo.svg';
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: '#eeeeee',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
